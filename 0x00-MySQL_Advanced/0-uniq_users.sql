4b2da14f8f034d84bd94d24de710f3de72a4aa1a
kalu_coder <awesomecoding51@gmail.com>
2023-04-08 20:52:10 +0300

    0th task resolved

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- a script that creates a table called users
-CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
