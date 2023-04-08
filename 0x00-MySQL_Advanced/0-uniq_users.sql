5403d41cccc8a7e1ad4f3c2d5058ed404f522a55
kalu_coder <awesomecoding51@gmail.com>
2023-04-08 15:47:33 +0300

    never null attribute added to field

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table called users
 CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
